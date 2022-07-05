6cf1a88693c093df64e10fbd075d0610b557c697
imanberdiyev <108271493+imanberdiyev@users.noreply.github.com>
2022-07-05 19:21:56 +0600

    1.3

--- final_win.py
+++ final_win.py
@@ -1,31 +1,31 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit 
-from instr import*
-
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from instr import *
 
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         super().__init__()
+        self.exp=exp
         self.set_appear()
         self.initUI()
         self.connects()
         self.show()
-
     def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
+        self.setWindowTitle(txt_finalwin)
+        self.resize(win_width,win_height)
         self.move(win_x,win_y)
-
     def initUI(self):
-        self.index=QLabel(txt_index)
-        self.txt_workheart=QLabel(txt_workheart)
+        ind=(4*(self.exp.test1+self.exp.test2+self.exp.test3)-200)/10
+        ind1=txt_index+str(ind)
+        self.index=QLabel(ind1)
+        self.workheart=QLabel(txt_workheart)
         self.m_line=QVBoxLayout()
-        self.m_line.addWidget(self.index, alignment = Qt.AlignCenter)
-        self.m_line.addWidget(self.txt_workheart, alignment =Qt.AlignCenter)
+        self.m_line.addWidget(self.index, alignment=Qt.AlignCenter)
+        self.m_line.addWidget(self.workheart, alignment=Qt.AlignCenter)
         self.setLayout(self.m_line)
 
     def connects(self):
         pass
-        
+
     def next_click(self):
-        pass
+        pass   
