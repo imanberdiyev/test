55f1ea969494584dc6d9ca3fcfad78c75acb845f
imanberdiyev <108271493+imanberdiyev@users.noreply.github.com>
2022-07-05 19:27:15 +0600

    1.3

--- my_app.py
+++ my_app.py
@@ -1,7 +1,7 @@
-from PyQt5.QtCore import Qt, QTimer, QTimer
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit 
-from instr import*
-from second_win import*
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from instr import *
+from second_win import *
 
 class MainWin(QWidget):
     def __init__(self):
@@ -10,34 +10,34 @@ class MainWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
-
+    
     def set_appear(self):
         self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
+        self.resize(win_width,win_height)
         self.move(win_x,win_y)
 
     def initUI(self):
-        self.hello_txt=QLabel(txt_hello)
-        self.intsruction=QLabel(txt_instruction)
-        self.button=QPushButton(txt_next)
+        self.hello_txt=QLabel(txt_hello)         #для текста добро пожаловать
+        self.instruction=QLabel(txt_instruction) # для текста инструкции
+        self.button=QPushButton(txt_next)        # для кнопки "начать"
 
-        self.Vlayout=QVBoxLayout()
-        '''добавление в вертикальное расположение виджетов'''
+        self.Vlayout=QVBoxLayout()   # для вертикального расположение виджетов 
+        ''' добавление в вертикальное расположение виджетов '''
         self.Vlayout.addWidget(self.hello_txt)
-        self.Vlayout.addWidget(self.intsruction)
-        self.Vlayout.addWidget(self.button)
+        self.Vlayout.addWidget(self.instruction)
+        self.Vlayout.addWidget(self.button) 
 
-        self.setLayout(self.Vlayout)
+        self.setLayout(self.Vlayout) # установить расположение для окна 
 
 
     def connects(self):
         self.button.clicked.connect(self.next_click)
-    
+
     def next_click(self):
         self.hide()
-        self.tw=TestWin()
-   
-        
+        self.tw=TestWin()    
+
+
 app=QApplication([])
 main_win=MainWin()
 app.exec_()
